59fd6f46fe790a35f1429ea1d3958ae023380f53
Emma Lundström <1emma.lundstrom@gmail.com>
2025-02-25 22:47:31 +0200

    5.7 (5.6. was done with 5.5)

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,27 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [blogVisible, setBlogVisible] = useState(false)
+
+  if (blogVisible === false) {
+    return (
+      <div className="blog">
+      {blog.title} {blog.author}
+      <button onClick={() => setBlogVisible(true)}>view</button>
+    </div>  
+    )
+  }
+
+  if (blogVisible === true) {
+    return (
+      <div className="blog">
+        <p>{blog.title} {blog.author}<button onClick={() => setBlogVisible(false)}>hide</button></p>
+        <p>{blog.url}</p>
+        <p>{blog.likes} <button>like</button></p>
+        <p>{blog.user.name}</p>
+      </div>
+    )
+  }
+}
 
 export default Blog
